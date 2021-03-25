b421528d313cee923434d7d46f55bc90764f5c3c
fnanni-0 <fnanni@itba.edu.ar>
2021-03-25 01:00:19 -0300

    refactor: increase delay to 60 min

--- src/bots/proof-of-humanity/index.js
+++ src/bots/proof-of-humanity/index.js
@@ -15,7 +15,7 @@ module.exports = async (web3, batchedSend) => {
   )
   const graph = new GraphQLClient(process.env.PROOF_OF_HUMANITY_SUBGRAPH_URL)
 
-  // Run every 5 minutes.
+  // Run every 60 minutes.
   while (true) {
     batchedSend(
       (
@@ -30,6 +30,6 @@ module.exports = async (web3, batchedSend) => {
         )
       ).flat()
     )
-    await delay(300000)
+    await delay(60 * 60 * 1000)
   }
 }
