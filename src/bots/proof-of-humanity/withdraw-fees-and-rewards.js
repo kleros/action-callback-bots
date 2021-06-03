02aab3d625e3fd9c1c7ac9b20f2a566e3a579fc8
michaeltimothym <Michael@kleros.io>
2021-06-03 17:21:00 -0400

    initial commit

--- src/bots/proof-of-humanity/withdraw-fees-and-rewards.js
+++ src/bots/proof-of-humanity/withdraw-fees-and-rewards.js
@@ -7,64 +7,48 @@ module.exports = async (graph, proofOfHumanity) => {
 
   let lastContributionID = "";
   let allContributions = [];
-  while (true) {
-    const { contributions } = await graph.request(
+  const { contributions } = await graph.request(
       gql`
         query withdrawFeesAndRewardsQuery($lastId: String) {
-          contributions(where: { values_not: [0, 0], id_gt: $lastId }, first: 1000) {
+          contributions(where: { values_not: [0, 0] }, first: 10) {
             contributor
             id
+            requestIndex
+            roundIndex
             round {
               id
               challenge {
                 request {
-                  resolved
                   submission {
                     id
-                    requests(orderBy: creationTime) {
-                      id
-                    }
                   }
                   id
                   challenges(orderBy: creationTime) {
                     id
                   }
                 }
-                id
-                rounds(orderBy: creationTime) {
-                  id
-                }
               }
             }
           }
         }
-      `,
-      {
-        lastId: lastContributionID,
-      }
-    )
-    allContributions = allContributions.concat(contributions)
-    if (contributions.length < 1000) break
-    lastContributionID = contributions[contributions.length-1].id
-  }
+      `
+  )
   return (
-    allContributions
-      // The request must be resolved.
-      .filter(contribution => contribution.round.challenge.request.resolved)
-      .map(({ contributor, round }) => ({
-        args: [
-          contributor,
-          round.challenge.request.submission.id,
-          round.challenge.request.submission.requests.findIndex(
-            request => request.id === round.challenge.request.id
-          ),
-          round.challenge.request.challenges.findIndex(
-            challenge => challenge.id === round.challenge.id
-          ),
-          round.challenge.rounds.findIndex(_round => _round.id === round.id)
-        ],
-        method: proofOfHumanity.methods.withdrawFeesAndRewards,
-        to: proofOfHumanity.options.address
-      }))
+      contributions
+          // The request must be resolved.
+          .filter(contribution => contribution.round.challenge.request.resolved)
+          .map(({ contributor, round }) => ({
+            args: [
+              contributor,
+              round.challenge.request.submission.id,
+              requestIndex,
+              round.challenge.request.challenges.findIndex(
+                  challenge => challenge.id === round.challenge.id
+              ),
+              roundIndex
+            ],
+            method: proofOfHumanity.methods.withdrawFeesAndRewards,
+            to: proofOfHumanity.options.address
+          }))
   )
 }
