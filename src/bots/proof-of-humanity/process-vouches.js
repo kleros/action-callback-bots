e6ee8ac0719223a4ed3ea235bd57a4ec3d3e015d
Matheus Alencar <matheus@kleros.io>
2021-06-09 11:12:54 +0400

    fix: use vouchReleaseReady to detect vouches to process

--- src/bots/proof-of-humanity/process-vouches.js
+++ src/bots/proof-of-humanity/process-vouches.js
@@ -5,66 +5,23 @@ const { gql } = require('graphql-request')
 // - The request must be resolved.
 // - The penalty index can't exceed the number of vouches.
 module.exports = async (graph, proofOfHumanity) => {
-  const {
-    contract: { requiredNumberOfVouches }
-  } = await graph.request(
+  const { submissions: allSubmissions } = await graph.request(
     gql`
-      query contractVariablesQuery {
-        contract(id: 0) {
-          requiredNumberOfVouches
+      query processVouchesQuery() {
+        submissions(first: 1000, where: { vouchReleaseReady: true }) {
+          vouchees (first: 100, where: { submissionTime_not: null } ) {
+            id
+            requestsLength
+          }
         }
       }
     `
   )
 
-  let lastSubmisionID = "";
-  let allSubmissions = [];
-  while (true) {
-    const { submissions } = await graph.request(
-      gql`
-        query processVouchesQuery($lastId: String) {
-          # require(request.resolved, "Submission should be resolved");
-          # Use id_gt instead of skip for better performance.
-          submissions(where: { status: "None", id_gt: $lastId }, first: 1000) {
-            id
-            requests(orderBy: creationTime) {
-              resolved
-              disputed
-              vouches {
-                id
-              }
-              penaltyIndex
-            }
-          }
-        }
-      `,
-      {
-        lastId: lastSubmisionID,
-      }
-    )
-    allSubmissions = allSubmissions.concat(submissions)
-    if (submissions.length < 1000) break
-    lastSubmisionID = submissions[submissions.length-1].id
-  }
+  return allSubmissions.map(({ vouchees: { id, requestsLength } }) => ({
+    args: [id, requestsLength - 1, 15],
+    method: proofOfHumanity.methods.processVouches,
+    to: proofOfHumanity.options.address,
+  }))
 
-  // AUTO_PROCESSED_VOUCH vouches are already processed when an undisputed request gets executed.
-  const AUTO_PROCESSED_VOUCH = 10
-  const vouchesAlreadyProcessed = requiredNumberOfVouches <= AUTO_PROCESSED_VOUCH
-
-  return allSubmissions.flatMap(({ id, requests }) =>
-    requests
-      // The request must be resolved.
-      // The penalty index can't exceed the number of vouches.
-      .map(
-        (request, index) =>
-          request.resolved &&
-          request.penaltyIndex < request.vouches.length && 
-          (request.disputed || !vouchesAlreadyProcessed) && {
-            args: [id, index, 15],
-            method: proofOfHumanity.methods.processVouches,
-            to: proofOfHumanity.options.address,
-          }
-      )
-      .filter(Boolean)
-  )
 }
