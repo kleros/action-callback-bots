172b5b98f52fd7d6700d11f2ae2cd226bec0e43b
Henrique Barcelos <hbarcelos909@gmail.com>
2021-06-14 12:10:53 -0300

    feat: introduced the bot for the Court on xDAI

--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
 const Web3 = require('web3')
 const delay = require('delay')
 const _batchedSend = require('./utils/batched-send')
+
 const bots = [
   process.env.KLEROS_LIQUID_CONTRACT_ADDRESS && require('./bots/kleros-liquid'),
-  process.env.PROOF_OF_HUMANITY_CONTRACT_ADDRESS && process.env.PROOF_OF_HUMANITY_SUBGRAPH_URL && require('./bots/proof-of-humanity'),
+  process.env.PROOF_OF_HUMANITY_CONTRACT_ADDRESS &&
+    process.env.PROOF_OF_HUMANITY_SUBGRAPH_URL &&
+    require('./bots/proof-of-humanity'),
   process.env.T2CR && require('./bots/t2cr'),
   process.env.BADGE_TCRS && require('./bots/badges')
 ]
 
+const xDaiBots = [
+  process.env.XDAI_X_KLEROS_LIQUID_CONTRACT_ADDRESS &&
+    require('./xdai-bots/x-kleros-liquid')
+]
+
 // Run bots and restart them on failures.
-const run = async bot => {
+const run = async (bot, providerUrl) => {
   if (bot) {
     // Create an instance of `web3` and `batched-send` for each bot.
-    const web3 = new Web3(process.env.WEB3_PROVIDER_URL)
+    const web3 = new Web3(providerUrl)
     const batchedSend = _batchedSend(
       web3,
       process.env.TRANSACTION_BATCHER_CONTRACT_ADDRESS,
@@ -30,4 +38,6 @@ const run = async bot => {
     }
   }
 }
-bots.forEach(run)
+
+bots.forEach(bot => run(bot, process.env.WEB3_PROVIDER_URL))
+xDaiBots.forEach(bot => run(bot, process.env.XDAI_WEB3_PROVIDER_URL))
---
+++ src/xdai-bots/x-kleros-liquid.js
@@ -0,0 +1,178 @@
+const delay = require('delay')
+const _xKlerosLiquid = require('../contracts/x-kleros-liquid.json')
+
+const DELAYED_STAKES_ITERATIONS = 15
+
+module.exports = async (web3, batchedSend) => {
+  // Instantiate the Kleros Liquid contract.
+  const klerosLiquid = new web3.eth.Contract(
+    _xKlerosLiquid.abi,
+    process.env.XDAI_X_KLEROS_LIQUID_CONTRACT_ADDRESS
+  )
+  const PhaseEnum = Object.freeze({ staking: 0, generating: 1, drawing: 2 })
+
+  // Keep track of executed disputes so we don't waste resources on them.
+  const executedDisputeIDs = {}
+
+  // Run every 5 minutes.
+  while (true) {
+    // Try to execute delayed set stakes if there are any. We check because this transaction still succeeds when there are not any and we don't want to waste gas in those cases.
+    if (
+      (await klerosLiquid.methods.lastDelayedSetStake().call()) >=
+      (await klerosLiquid.methods.nextDelayedSetStake().call())
+    )
+      batchedSend({
+        args: [DELAYED_STAKES_ITERATIONS],
+        method: klerosLiquid.methods.executeDelayedSetStakes,
+        to: klerosLiquid.options.address
+      })
+
+    // Loop over all disputes.
+    try {
+      let disputeID = 0
+      while (true) {
+        if (!executedDisputeIDs[disputeID]) {
+          const dispute = await klerosLiquid.methods.disputes(disputeID).call()
+          const dispute2 = await klerosLiquid.methods
+            .getDispute(disputeID)
+            .call()
+          const voteCounters = await Promise.all(
+            // eslint-disable-next-line no-loop-func
+            dispute2.votesLengths.map(async (numberOfVotes, i) => {
+              let voteCounter
+              try {
+                voteCounter = await klerosLiquid.methods
+                  .getVoteCounter(disputeID, i)
+                  .call()
+              } catch (_) {
+                // Look it up manually if numberOfChoices is too high for loop
+                let tied = true
+                let winningChoice = '0'
+                const _voteCounters = {}
+
+                for (let j = 0; j < numberOfVotes; j++) {
+                  const vote = await klerosLiquid.methods.getVote(
+                    disputeID,
+                    i,
+                    j
+                  )
+                  if (vote.voted) {
+                    // increment vote count
+                    _voteCounters[vote.choice] = _voteCounters[vote.choice]
+                      ? _voteCounters[vote.choice] + 1
+                      : 1
+                    if (vote.choice === winningChoice) {
+                      if (tied) tied = false // broke tie
+                    } else {
+                      const _winningChoiceVotes =
+                        _voteCounters[winningChoice] || 0
+                      if (_voteCounters[vote.choice] > _winningChoiceVotes) {
+                        winningChoice = vote.choice
+                        tied = false
+                      } else if (
+                        _voteCounters[vote.choice] === _winningChoiceVotes
+                      )
+                        tied = true
+                    }
+                  }
+                }
+
+                voteCounter = {
+                  tied,
+                  winningChoice
+                }
+              }
+
+              return voteCounter
+            })
+          )
+
+          const notTieAndNoOneCoherent = voteCounters.map(
+            v =>
+              !voteCounters[voteCounters.length - 1].tied &&
+              v.counts[voteCounters[voteCounters.length - 1].winningChoice] ===
+                '0'
+          )
+          if (
+            !dispute.ruled ||
+            dispute2.votesLengths.some(
+              (l, i) =>
+                Number(notTieAndNoOneCoherent[i] ? l : l * 2) !==
+                Number(dispute2.repartitionsInEachRound[i])
+            )
+          )
+            // The dispute is not finalized, try to call all of its callbacks.
+            batchedSend(
+              [
+                // We check if there are still pending draws because if there aren't any and the dispute is still in the evidence period,
+                // then the transaction would still succeed and we don't want to waste gas in those cases.
+                dispute2.votesLengths[dispute2.votesLengths.length - 1] >
+                  dispute.drawsInRound && {
+                  args: [disputeID, 15],
+                  method: klerosLiquid.methods.drawJurors,
+                  to: klerosLiquid.options.address
+                },
+                ...dispute2.votesLengths.map(
+                  // eslint-disable-next-line no-loop-func
+                  (l, i) =>
+                    Number(notTieAndNoOneCoherent[i] ? l : l * 2) !==
+                      Number(dispute2.repartitionsInEachRound[i]) && {
+                      args: [disputeID, i, 15],
+                      method: klerosLiquid.methods.execute,
+                      to: klerosLiquid.options.address
+                    }
+                ),
+                {
+                  args: [disputeID],
+                  method: klerosLiquid.methods.executeRuling,
+                  to: klerosLiquid.options.address
+                },
+                {
+                  args: [disputeID],
+                  method: klerosLiquid.methods.passPeriod,
+                  to: klerosLiquid.options.address
+                }
+              ].filter(t => t)
+            )
+          else executedDisputeIDs[disputeID] = true // The dispute is finalized, cache it.
+        }
+        disputeID++
+      }
+    } catch (_) {} // Reached the end of the disputes list.
+
+    // Try to pass the phase.
+    let readyForNextPhase = false
+    const phase = await klerosLiquid.methods.phase().call()
+    const lastPhaseChange = await klerosLiquid.methods.lastPhaseChange().call()
+    const disputesWithoutJurors = await klerosLiquid.methods
+      .disputesWithoutJurors()
+      .call()
+    if (phase == PhaseEnum.staking) {
+      const minStakingTime = await klerosLiquid.methods.minStakingTime().call()
+      if (
+        Date.now() - lastPhaseChange * 1000 >= minStakingTime * 1000 &&
+        disputesWithoutJurors > 0
+      ) {
+        readyForNextPhase = true
+      }
+    } else if (phase == PhaseEnum.generating) {
+      readyForNextPhase = true
+    } else if (phase == PhaseEnum.drawing) {
+      const maxDrawingTime = await klerosLiquid.methods.maxDrawingTime().call()
+      if (
+        Date.now() - lastPhaseChange * 1000 >= maxDrawingTime * 1000 &&
+        disputesWithoutJurors == 0
+      ) {
+        readyForNextPhase = true
+      }
+    }
+
+    if (readyForNextPhase) {
+      batchedSend({
+        method: klerosLiquid.methods.passPhase,
+        to: klerosLiquid.options.address
+      })
+    }
+    await delay(5 * 60 * 1000)
+  }
+}
