80688fa4b98d0221e1535fa5dd069cc3b4f5e970
Henrique Barcelos <hbarcelos909@gmail.com>
2021-06-25 16:43:39 -0300

    refactor: introduced feature toggle for xDAI bots

--- src/index.js
+++ src/index.js
@@ -10,34 +10,44 @@ const bots = [
   process.env.T2CR && require('./bots/t2cr'),
   process.env.BADGE_TCRS && require('./bots/badges')
 ]
+  .filter(bot => typeof bot === 'function')
 
 const xDaiBots = [
-  process.env.XDAI_X_KLEROS_LIQUID_CONTRACT_ADDRESS &&
+  process.env.XDAI_ENABLED === 'true' &&
+    process.env.XDAI_X_KLEROS_LIQUID_CONTRACT_ADDRESS &&
     require('./xdai-bots/x-kleros-liquid')
 ]
+  .filter(bot => typeof bot === 'function')
 
 // Run bots and restart them on failures.
-const run = async (bot, providerUrl) => {
-  if (bot) {
-    // Create an instance of `web3` and `batched-send` for each bot.
-    const web3 = new Web3(providerUrl)
-    const batchedSend = _batchedSend(
-      web3,
-      process.env.TRANSACTION_BATCHER_CONTRACT_ADDRESS,
-      process.env.BATCH_SEND_PRIVATE_KEY,
-      20000 // Batch time window of 20 seconds.
-    )
+const run = async (bot, { providerUrl, batcherAddress, privateKey }) => {
+  // Create an instance of `web3` and `batched-send` for each bot.
+  const web3 = new Web3(providerUrl)
+  const batchedSend = _batchedSend(
+    web3,
+    batcherAddress,
+    privateKey,
+    20000 // Batch time window of 20 seconds.
+  )
 
-    while (true) {
-      try {
-        await bot(web3, batchedSend)
-      } catch (err) {
-        console.error('Bot error: ', err)
-      }
-      await delay(10000) // Wait 10 seconds before restarting failed bot.
+  while (true) {
+    try {
+      await bot(web3, batchedSend)
+    } catch (err) {
+      console.error('Bot error: ', err)
     }
+    await delay(10000) // Wait 10 seconds before restarting failed bot.
   }
 }
 
-bots.forEach(bot => run(bot, process.env.WEB3_PROVIDER_URL))
-xDaiBots.forEach(bot => run(bot, process.env.XDAI_WEB3_PROVIDER_URL))
+bots.forEach(bot => run(bot, {
+  providerUrl: process.env.WEB3_PROVIDER_URL,
+  batcherAddress: process.env.TRANSACTION_BATCHER_CONTRACT_ADDRESS,
+  privateKey: process.env.BATCH_SEND_PRIVATE_KEY,
+}))
+
+xDaiBots.forEach(bot => run(bot, {
+  providerUrl: process.env.XDAI_WEB3_PROVIDER_URL,
+  batcherAddress: process.env.XDAI_TRANSACTION_BATCHER_CONTRACT_ADDRESS,
+  privateKey: process.env.XDAI_BATCH_SEND_PRIVATE_KEY
+}))
